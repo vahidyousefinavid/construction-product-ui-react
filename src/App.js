6dd1131bbc265b12939bc75a855a581f78e89975
ali azizi <derehbeg@gmail.com>
2023-12-24 12:59:57 +0330

    add fooer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import NavbarMain from './Layout/Navbar/NavbarMain'
-import ImageHero from './Layout/ImageHero/ImageHero'
-import Ht from './Layout/ListHesderText/Ht'
-import ListH from './Layout/ListHero/ListH'
-import Slider from './Layout/Caruosel/Slider'
-import Achieve from './Layout/Achivement/Achieve'
-import SlideT from './Layout/slideText/SlideT'
-import Footer from './Layout/Footer/Footer'
+import React from "react";
+import NavbarMain from "./Layout/Navbar/NavbarMain";
+import ImageHero from "./Layout/ImageHero/ImageHero";
+import Ht from "./Layout/ListHesderText/Ht";
+import ListH from "./Layout/ListHero/ListH";
+import Slider from "./Layout/Caruosel/Slider";
+import Achieve from "./Layout/Achivement/Achieve";
+import SlideT from "./Layout/slideText/SlideT";
+import Footer from "./Layout/Footer/Footer";
+import { Route, Routes } from "react-router";
 
 const App = () => {
   return (
-    <div>
-     <NavbarMain/>
-     <ImageHero/>
-     <Ht/>
-     <ListH/>
-     <Slider/>
-     <Achieve/>
-     <SlideT/>
-     <Footer/>
-    </div>
-  )
-}
+    <>
+   
+        
+        <NavbarMain />
+        <ImageHero />
+        <Ht />
+        <ListH />
+        <Slider />
+        <Achieve />
+        <SlideT />
+        <Footer />
+        {/* <Routes>
+          <Route/>
+        </Routes> */}
+      
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/Layout/Navbar/NavbarMain.js
+++ src/Layout/Navbar/NavbarMain.js
@@ -1,31 +1,63 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+// import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 import Logo from "./../../image/Logo.svg";
-import './NavbarMain.css'
+import { Link } from "react-router-dom";
+import "./NavbarMain.css";
 const NavbarMain = () => {
   return (
     <div>
       <div className="Container">
-        <Navbar expand="lg" className=" font-iransans" >
+        <Navbar expand="lg" className=" font-iransans">
           {/* <Container> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-around sm:z-0">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-around sm:z-0"
+          >
             <div className="col-lg-1 text-center">
               <span>فارسی/انگلیسی</span>
             </div>
             <div className="col-lg-8">
               <Nav className="me-auto justify-center">
-                <Nav.Link href="#home" style={{color:"#000",fontWeight:"bold"}}>خانه</Nav.Link>
-                <Nav.Link href="#link" style={{color:"#000",fontWeight:"bold"}}>دیده شوید</Nav.Link>
-                <Nav.Link href="#link" style={{color:"#000",fontWeight:"bold"}}>ارتباط با ما</Nav.Link>
-                <Nav.Link href="#link" style={{color:"#000",fontWeight:"bold"}}>درباره ما</Nav.Link>
-                <Nav.Link href="#link" style={{color:"#000",fontWeight:"bold"}}>پنل فروشکاه</Nav.Link>
-                <Nav.Link href="#link" style={{color:"#000",fontWeight:"bold"}}>نقضه راه</Nav.Link>
-
-              
+                <Nav.Link
+                  href="#home"
+                  style={{ color: "#000", fontWeight: "bold" }}
+                >
+                  خانه
+                </Nav.Link>
+                <Nav.Link
+                  href="#link"
+                  style={{ color: "#000", fontWeight: "bold" }}
+                >
+                  دیده شوید
+                </Nav.Link>
+                
+                <Nav.Link
+                  href="#link"
+                  style={{ color: "#000", fontWeight: "bold" }}
+                >
+                  درباره ما
+                  
+                </Nav.Link>
+                <Nav.Link
+                  href="#link"
+                  style={{ color: "#000", fontWeight: "bold" }}
+                ></Nav.Link>
+                <Nav.Link
+                  href="#link"
+                  style={{ color: "#000", fontWeight: "bold" }}
+                >
+                  پنل فروشکاه
+                </Nav.Link>
+                <Nav.Link
+                  href="#link"
+                  style={{ color: "#000", fontWeight: "bold" }}
+                >
+                  نقشه راه
+                </Nav.Link>
               </Nav>
             </div>
             <div className="col-lg-1">
